95ab4b711769a11dc482f691281a60582593dbbe
Prashanth Puranik <puranik@digdeeper.in>
2022-05-15 11:22:47 +0530

    other buttons (exercise)

--- demos/week-05-may-15/basic-calculator/index.js
+++ demos/week-05-may-15/basic-calculator/index.js
@@ -3,9 +3,23 @@ function displaySymbol( event ) {
     const button = event.target;
 
     // the text within the button
-    const buttonText = event.target.innerText;
+    const buttonText = button.innerText;
 
     // put this text in the .input-box (use querySelector)
     const inputBox = document.querySelector( '.input-box' );
-    inputBox.innerText += buttonText;
+    inputBox.innerText = inputBox.innerText + buttonText;
+}
+
+function cancel() {
+    // clear the input-box
+}
+
+function backspace() {
+    // use substring() function on the input-box innerText
+}
+
+function calculate() {
+    const inputBox = document.querySelector( '.input-box' );
+    const expr = inputBox.innerText;
+    inputBox.innerText = eval( expr );
 }
