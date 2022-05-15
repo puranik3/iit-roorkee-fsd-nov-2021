e84db843b75fbe224c7dd3606b67e5c9086928ce
Prashanth Puranik <puranik@digdeeper.in>
2022-05-15 10:51:37 +0530

    started with JS for basic calculator

---
+++ demos/week-05-may-15/basic-calculator/index.js
@@ -0,0 +1,7 @@
+function displaySymbol( event ) {
+    const button = event.target; // button that was clicked
+    const buttonText = event.target.innerText; // the text within the button
+
+    // put this text in the .input-box
+    // exercise
+}
