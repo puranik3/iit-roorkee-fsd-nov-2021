298dcbe2056be73f4b72de4a16abc137d6c0a148
Prashanth Puranik <puranik@digdeeper.in>
2022-05-22 11:25:11 +0530

    Quiz class

--- demos/week-07-may-22/index.js
+++ demos/week-07-may-22/index.js
@@ -13,12 +13,25 @@ Question.prototype.isCorrectAnswer = function( answer ) {
  * Create the Quiz class
  * - questions (input in constructor - array of questions)
  * - score (initially 0)
- * - questionIndex (initially 0)
+ * - questionIndex (initially 0) - represents the index of the current question in the questions array
  * 
  * Methods
- * getQuestionByIndex - return the question object for questionIndex
+ * getCurrentQuestion - return the question object for questionIndex
  * checkOptionWithAnswer - answer is checked against correct answer of current question - true of false is returned - please use the question object's isCorrectAnswer method
  */
+function Quiz( questions ) {
+    this.questions = questions;
+    this.score = 0;
+    this.questionIndex = 0;
+}
+
+Quiz.prototype.getCurrentQuestion = function() {
+    return this.questions[this.questionIndex];
+};
+
+Quiz.prototype.checkOptionWithAnswer = function( answer ) {
+    return this.getCurrentQuestion().isCorrectAnswer( answer );
+};
 
 const questions = [
     new Question("JavaScript supports", ["Functions", "XHTML","CSS", "HTML"], "Functions"),
@@ -33,3 +46,10 @@ console.log( questions );
 // If implemented correctly this should be the output
 console.log( questions[0].isCorrectAnswer( "Functions" ) ); // true
 console.log( questions[0].isCorrectAnswer( "CSS" ) ); // false
+
+const quiz = new Quiz( questions );
+console.log( quiz );
+
+console.log( quiz.getCurrentQuestion() );
+console.log( quiz.checkOptionWithAnswer( 'Functions' ) ); // true
+console.log( quiz.checkOptionWithAnswer( 'CSS' ) ); // false
