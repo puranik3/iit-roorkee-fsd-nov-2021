e429f008be5d49ccb5ef343c494c76e388b836f7
Prashanth Puranik <puranik@digdeeper.in>
2022-06-05 12:56:06 +0530

    able to add to cart

--- demos/week-08-jun-05/index.js
+++ demos/week-08-jun-05/index.js
@@ -1,8 +1,18 @@
 import { foodItems } from './food-items.js';
 
+const cart = [];
+
 const filterByCategory = ( category ) => {
     return foodItems.filter( item => item.category === category );
 };
+
+const findById = ( itemId ) => {
+    return foodItems.find( item => item.id === itemId );
+};
+
+const findItemInCart = ( itemId ) => {
+    return cart.find( cartItem => cartItem.item.id === itemId );
+}
 
 // array of categories with duplicates
 const categoriesArrayDuplicates = foodItems.map( item => item.category );
@@ -22,10 +32,10 @@ const showItemsByCategory = () => {
             filterByCategory( category ).forEach(
                 item => {
                     itemsHTML += `
-                        <div class="category-item">
+                        <div class="category-item" data-id="${item.id}">
                             <div class="category-item-actions">
                                 <i class="fa-solid fa-star"> ${item.rating}</i>
-                                <i class="fa-solid fa-heart"></i>
+                                <i class="fa-solid fa-heart add-item-to-cart"></i>
                             </div>
                             <img src="${item.img}" alt="${item.name}" />
                             <div>${item.name}</div>
@@ -35,7 +45,7 @@ const showItemsByCategory = () => {
                 }
             );
 
-            console.log( itemsHTML );
+            // console.log( itemsHTML );
 
             // render HTML for a category and its items
             main.innerHTML += `
@@ -49,3 +59,42 @@ const showItemsByCategory = () => {
 }
 
 showItemsByCategory();
+
+function bindListeners() {
+    const addItemToCartButtons = document.querySelectorAll( '.add-item-to-cart' );
+
+    // console.log( addItemToCartButtons );
+
+    addItemToCartButtons.forEach(
+        button => {
+            button.addEventListener( 'click', function() {
+                // the button (icon) which was clicked
+                // console.log( this );
+
+                const itemEl = this.closest( '.category-item' );
+                // console.log( itemEl );
+
+                const itemId = parseInt( itemEl.getAttribute( 'data-id' ) );
+                console.log( itemId );
+
+                const item = findById( itemId );
+                console.log( item );
+
+                // check if item exists in the cart
+                if( findItemInCart( itemId ) ) {
+                    alert( 'This items is already in the cart' );
+                } else {
+                    cart.push({
+                        qty: 1,
+                        // item: item
+                        item
+                    });
+                }
+
+                console.log( cart );
+            });
+        }
+    )
+}
+
+bindListeners();
