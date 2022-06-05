3444866b0c283cf36bcdb0979088a53ae55c4df3
Prashanth Puranik <puranik@digdeeper.in>
2022-06-05 11:13:04 +0530

    filtering by category

--- demos/week-08-jun-05/index.js
+++ demos/week-08-jun-05/index.js
@@ -1,10 +1,15 @@
 import { foodItems } from './food-items.js';
 
-// array of categories with dupliactes
+const filterByCategory = ( category ) => {
+    return foodItems.filter( item => item.category === category );
+};
+
+// array of categories with duplicates
 const categoriesArrayDuplicates = foodItems.map( item => item.category );
 const categoriesSet = new Set( categoriesArrayDuplicates );
 
 // array of categories without duplicates
 const categories = Array.from( categoriesSet );
 
-console.log( categories );
+console.log( filterByCategory( 'biryani' ) );
+console.log( filterByCategory( 'paneer' ) );
