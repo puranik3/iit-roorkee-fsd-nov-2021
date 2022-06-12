9aff5d48e3afa6e66bd0cef200a876c690320683
Prashanth Puranik <puranik@digdeeper.in>
2022-06-12 13:14:59 +0530

    handling data and error

--- demos/week-09-jun-12/index.js
+++ demos/week-09-jun-12/index.js
@@ -3,7 +3,21 @@ const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 function getWeather( city ) {
     fetch( `${baseUrl}?q=${city}&appid=${appid}&units=metric` )
-        .then( response => console.log( response ) )
+        .then( response => {
+            console.log( response );
+
+            // throw error is server returns 4xx response
+            if( !response.ok ) {
+                throw new Error( response.statusText );
+            }
+
+            return response.json();
+        })
+        .then( data => {
+            console.log( data );
+
+            // display weather data
+        })
         .catch( error => console.log( error.message ) );
 }
 
