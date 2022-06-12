944f838730835e72a8b6ffaf04de9f87a22c1bf5
Prashanth Puranik <puranik@digdeeper.in>
2022-06-12 13:33:37 +0530

    weather data is dsiplayed

--- demos/week-09-jun-12/index.js
+++ demos/week-09-jun-12/index.js
@@ -6,7 +6,7 @@ function getWeather( city ) {
         .then( response => {
             console.log( response );
 
-            // throw error is server returns 4xx response
+            // throw error if server returns 4xx response
             if( !response.ok ) {
                 throw new Error( response.statusText );
             }
@@ -17,8 +17,30 @@ function getWeather( city ) {
             console.log( data );
 
             // display weather data
+            showWeather( data );
         })
         .catch( error => console.log( error.message ) );
 }
 
 getWeather( 'Bangalore' );
+
+function showWeather( data ) {
+    const city = document.querySelector( '.city' );
+    const date = document.querySelector( '.date' );
+    const temp = document.querySelector( '.temp' );
+    const weather = document.querySelector( '.weather' );
+    const hilow = document.querySelector( '.hi-low' );
+
+    city.textContent = data.name;
+    date.textContent = getFormattedDate( new Date( data.dt * 1000 ) );
+    temp.innerHTML = `${data.main.temp} <span>°c</span>`;
+    weather.textContent = data.weather[0].main;
+    hilow.textContent = `${data.main.temp_min} °c / ${data.main.temp_max} °c`;
+}
+
+function getFormattedDate( date ) {
+    const days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
+    const months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+
+    return `${days[date.getDay()]} ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+}
