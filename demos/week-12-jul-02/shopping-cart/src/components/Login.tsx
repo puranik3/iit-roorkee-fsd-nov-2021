216398ff22b5fea33e62ececb8c24b3856b97040
Prashanth Puranik <puranik@digdeeper.in>
2022-07-03 11:43:33 +0530

    login complete

--- demos/week-12-jul-02/shopping-cart/src/components/Login.tsx
+++ demos/week-12-jul-02/shopping-cart/src/components/Login.tsx
@@ -1,6 +1,31 @@
+import { useRef, useState, FormEvent } from 'react';
 import { Card, Button, Form } from "react-bootstrap";
+import { useNavigate } from 'react-router-dom';
+
+import data from '../data';
 
 const Login = () => {
+    const usernameRef = useRef<HTMLInputElement>( null );
+    const passwordRef = useRef<HTMLInputElement>( null );
+    
+    const navigate = useNavigate();
+
+    const [ error, setError ] = useState( '' );
+
+    const doLogin = ( event : FormEvent<HTMLFormElement> ) => {
+        event.preventDefault();
+
+        const user = data.users.find(
+            user => user.username === usernameRef.current?.value && user.password === passwordRef.current?.value
+        );
+
+        if( !user ) {
+            setError( 'Credentials did not match' );
+        } else {
+            navigate( '/shopping' );
+        }
+    };
+
     return (
         <div
             className="d-flex flex-column justify-content-center align-items-center"
@@ -8,7 +33,12 @@ const Login = () => {
         >
             <h2 className="mb-3">Login</h2>
             <Card style={{ width: "20rem" }} className="p-4">
-                <Form>
+                {
+                    error && (
+                        <small className="text-danger">{error}</small>
+                    )
+                }
+                <Form onSubmit={doLogin}>
                     <Form.Group
                         className="mb-3"
                         controlId="formBasicEmail"
@@ -17,6 +47,7 @@ const Login = () => {
                         <Form.Control
                             type="text"
                             placeholder="Username"
+                            ref={usernameRef}
                         />
                     </Form.Group>
 
@@ -28,6 +59,7 @@ const Login = () => {
                         <Form.Control
                             type="password"
                             placeholder="Password"
+                            ref={passwordRef}
                         />
                     </Form.Group>
                     <Button variant="primary" type="submit">
