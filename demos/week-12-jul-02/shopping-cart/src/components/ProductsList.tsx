bb5df14cbd40ae18338a743149db3d7a482504e6
Prashanth Puranik <puranik@digdeeper.in>
2022-07-03 12:17:36 +0530

    Products list

--- demos/week-12-jul-02/shopping-cart/src/components/ProductsList.tsx
+++ demos/week-12-jul-02/shopping-cart/src/components/ProductsList.tsx
@@ -1,8 +1,28 @@
+import { Row, Col, Card, Button } from 'react-bootstrap';
+
+import data from "../data";
+
 const ProductsList = () => {
     return (
         <div>
             <h2>List of products</h2>
             <hr />
+            <Row xs={2} lg={4}>
+                {data.products.map((product) => (
+                    <Col key={product.id} className="my-2 d-flex align-items-stretch">
+                        <Card>
+                            <Card.Img variant="top" src={product.image} />
+                            <Card.Body>
+                                <Card.Title>{product.name}</Card.Title>
+                                <Card.Text>
+                                    Rs. {product.price}
+                                </Card.Text>
+                                <Button variant="primary">Add</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
         </div>
     );
 };
